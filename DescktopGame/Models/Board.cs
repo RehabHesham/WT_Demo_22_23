932e7f08745ca57422ae612307d0828e02ff3f35
Rehab Hesham <Rehab Hesham@DESKTOP-JCFIEDI>
2023-02-09 10:12:35 +0200

    Apply OOP on XO game

---
+++ DescktopGame/Models/Board.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescktopGame.Models
+{
+    internal class Board
+    {
+        private int[,] board;
+
+        public Board()
+        {
+            board = new int[3,3];
+        }
+
+        private bool IsAWinner(int player)
+        {
+            bool win = false;
+            // check rows
+            for (int i = 0; i < 3; i++)
+            {
+                win = true;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] != player) win = false;
+                }
+                if (win) { return true; }
+            }
+
+            // check columns
+            for (int i = 0; i < 3; i++)
+            {
+                win = true;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[j, i] != player) win = false;
+                }
+                if (win) { return true; }
+            }
+
+            // / diagonal
+            win = true;
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, i] != player) win = false;
+            }
+            if (win) { return true; }
+
+            // \ diagonal
+            win = true;
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 2 - i] != player) win = false;
+            }
+            if (win) { return true; }
+
+            return false;
+        }
+
+
+        private bool IsFull()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == 0) return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Applay player move on the board
+        /// </summary>
+        /// <param name="row">defines the row of the wanted cell</param>
+        /// <param name="col">defines the col of the wanted cell</param>
+        /// <param name="player">defines current player number</param>
+        /// <returns>
+        /// return int represent the game state
+        /// <list type="bullet">
+        /// <item>0 : for the draw case (no one wins).</item>
+        /// <item>1 : for winning case as current player wins</item>
+        /// <item>2 : for waiting the other player turn</item>
+        /// </list>
+        /// </returns>
+        public int ApplyMove(int row, int col, int player)
+        {
+            board[row, col] = player;
+            // wins  => return 1
+            if(IsAWinner(player)) { return 1; }
+            // Draw  => return 0
+            if(IsFull()) { return 0; }
+            // wait next move  => return 2
+            return 2;
+        }
+    }
+}
---
+++ DescktopGame/Models/Game.cs
@@ -0,0 +1,65 @@
+﻿namespace DescktopGame.Models
+{
+    internal class Game
+    {
+        public string playerXName;
+        public int playerXScore;
+
+        public string playerOName;
+        public int playerOScore;
+
+        public int currentPlayer;
+
+        private Board board;
+        public Game()
+        {
+            board = new Board();
+            currentPlayer = 1;
+            playerOScore = 0;
+            playerXScore = 0;
+        }
+
+        /// <summary>
+        /// Applay player move on the board
+        /// </summary>
+        /// <param name="row">defines the row of the wanted cell</param>
+        /// <param name="col">defines the col of the wanted cell</param>
+        /// <returns>
+        /// return int represent the game state
+        /// <list type="bullet">
+        /// <item>0 : for the draw case (no one wins).</item>
+        /// <item>1 : for winning case as current player wins</item>
+        /// <item>2 : for waiting the other player turn</item>
+        /// </list>
+        /// </returns>
+        public int ApplyMove(int row, int col)
+        {
+            int result = board.ApplyMove(row, col, currentPlayer);
+            switch (result)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    if (currentPlayer == 1) { playerXScore += 1; }
+                    else { playerOScore += 1; }
+                    return 1;
+                case 2:
+                    currentPlayer = currentPlayer == 1 ? 2 : 1;
+                    return 2;
+                default: return result;
+            }
+        }
+
+        public void NewGame()
+        {
+            board = new Board();
+            currentPlayer= 1;
+        }
+        public void Reset()
+        {
+            NewGame();
+            playerXScore= 0;
+            playerOScore= 0;
+        }
+    }
+}
